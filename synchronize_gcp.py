7bffde053119b92c7ac660a805b4a0e06b9b1b3f
Grzegorz <g.w.mika@gmail.com>
2020-04-19 19:05:18 +0200

    move to  using compressed binary files

--- synchronize_gcp.py
+++ synchronize_gcp.py
@@ -18,15 +18,27 @@ def establish_connection() -> storage.client.Client:
     return storage_client
 
 
-def list_files_gcp(client: storage.client.Client, bucket) -> List[str]:
+def list_files_gcp(client: storage.client.Client, bucket_name: str) -> List[str]:
     """
     List all files in a bucket specified by argument bucket.
+    :param bucket_name:
     :param client: connection instance to GCP service
     :param bucket: bucket name in which to list files
-    :return: list of file names
+    :return: list of file names (without extension)
     """
-    blobs = client.list_blobs(bucket)
-    files = [blob.name for blob in blobs]
+    blobs = client.list_blobs(bucket_name)
+    files = [blob.name.split('.')[0] for blob in blobs]
+    return files
+
+
+def list_files_local(path: str) -> List[str]:
+    """
+    List files in local directory specified by path and saved in parquet format.
+    :param path: path to the local directory containing files
+    :return: list of file names (without extension)
+    """
+    files = os.listdir(path)
+    files = [f.split('.')[0] for f in files if 'pkl' in f]
     return files
 
 
@@ -40,7 +52,7 @@ def compare_lists(local_list: List[str], remote_list: List[str]) -> List[str]:
     return list(set(local_list) - set(remote_list))
 
 
-def upload(client: storage.client.Client, file_names: List[str], bucket_name, path):
+def upload(client: storage.client.Client, file_names: List[str], bucket_name: str, path: str):
     """
     Upload a specified files to a given bucket in Google Cloud Storage instance.
     :param client: connection instance to GCP service
@@ -51,11 +63,11 @@ def upload(client: storage.client.Client, file_names: List[str], bucket_name, pa
     bucket = client.bucket(bucket_name)
     for file in file_names:
         try:
-            data = pd.read_csv(os.path.join(path, file))
+            data = pd.read_pickle(os.path.join(path, file + '.pkl.gz'), compression="gzip")
             f = StringIO()
             data.to_csv(f, index_label=False)
             f.seek(0)
-            blob = bucket.blob(file)
+            blob = bucket.blob(file + '.csv')
             blob.upload_from_file(f, content_type='text/csv')
         except Exception as e:
             logging.error(e)
@@ -65,14 +77,13 @@ def main():
     logging.basicConfig(filename='/home/database/log.log', level=logging.WARNING,
                         format='%(asctime)s %(levelname)s %(name)s %(message)s')
 
-    local_storage: str = '.'
+    local_storage: str = '/home/database'
     bucket_name: str = 'lilia'
 
     client: storage.client.Client = establish_connection()
 
-    local_list: List[str] = os.listdir(local_storage)
-    local_list = [f for f in local_list if 'txt' in f]
-    remote_list: List[str] = list_files_gcp(client, bucket=bucket_name)
+    local_list: List[str] = list_files_local(local_storage)
+    remote_list: List[str] = list_files_gcp(client, bucket_name=bucket_name)
     files_to_upload: List[str] = compare_lists(local_list, remote_list)
 
     upload(client, files_to_upload, bucket_name, local_storage)
