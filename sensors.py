4d4ea1115d91a1bacf9962ac03a1c2b376583457
Grzegorz <g.w.mika@gmail.com>
2020-05-21 23:32:59 +0200

    type hints

--- sensors.py
+++ sensors.py
@@ -1,34 +1,49 @@
+from abc import ABCMeta, abstractmethod
+
 from grove.grove_moisture_sensor import GroveMoistureSensor
 from grove.grove_light_sensor_v1_2 import GroveLightSensor
 from grove.seeed_dht import DHT
 
 
-class Moisture:
-    def __init__(self, port):
+class Sensor(metaclass=ABCMeta):
+    @abstractmethod
+    def __init__(self):
+        ...
+
+    @abstractmethod
+    @property
+    def measurement(self) -> float:
+        ...
+
+
+class Moisture(Sensor):
+    def __init__(self, port: int):
         """
         Wrapper for Grove Moisture sensor.
         :param port: port to which the sensor is connected
         """
+        super().__init__()
         self.sensor = GroveMoistureSensor(port)
 
     @property
-    def measurement(self):
+    def measurement(self) -> float:
         """
         Get value of the measurement
         """
         return self.sensor.moisture
 
 
-class Temperature:
-    def __init__(self, port):
+class Temperature(Sensor):
+    def __init__(self, port: int):
         """
         Wrapper for Grove Temperature sensor.
         :param port: port to which the sensor is connected
         """
+        super().__init__()
         self.sensor = DHT("11", port)
 
     @property
-    def measurement(self):
+    def measurement(self) -> float:
         """
         Get value of the measurement
         """
@@ -36,16 +51,17 @@ class Temperature:
         return temperature
 
 
-class Humidity:
-    def __init__(self, port):
+class Humidity(Sensor):
+    def __init__(self, port: int):
         """
         Wrapper for Grove Humidity sensor.
         :param port: port to which the sensor is connected
         """
+        super().__init__()
         self.sensor = DHT("11", port)
 
     @property
-    def measurement(self):
+    def measurement(self) -> float:
         """
         Get value of the measurement
         """
@@ -53,16 +69,17 @@ class Humidity:
         return humidity
 
 
-class Light:
-    def __init__(self, port):
+class Light(Sensor):
+    def __init__(self, port: int):
         """
         Wrapper for Grove Light sensor.
         :param port: port to which the sensor is connected
         """
+        super().__init__()
         self.sensor = GroveLightSensor(port)
 
     @property
-    def measurement(self):
+    def measurement(self) -> float:
         """
         Get value of the measurement
         """
